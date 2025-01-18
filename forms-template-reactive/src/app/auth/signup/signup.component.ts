355861217dab70c487d2c2112c06ca94b47eff69
RupasreeH <hroopa015@gmail.com>
2025-01-17 18:31:29 -0600

    committed changes for module complete

--- forms-template-reactive/src/app/auth/signup/signup.component.ts
+++ forms-template-reactive/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+function equalValues(controlName1:string, controlName2:string){
+return (control:AbstractControl) => {
+  const password = control.get('password')?.value;
+  const confirmPassword = control.get('confirmPassword')?.value;
+  
+  if(password === confirmPassword){
+    return null;
+  }
+  return {valuesNotEqual: true};
+  
+}
+
+}
 @Component({
   selector: 'app-signup',
   standalone: true,
@@ -13,14 +26,50 @@ export class SignupComponent {
     email: new FormControl('',{
       validators:[Validators.email, Validators.required]
     }),
-    passWord: new FormControl('',{
-      validators:[Validators.required, Validators.minLength(6)],
-    })
-  });
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators:[Validators.required, Validators.minLength(6)],
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+    },
+    {
+      validators:[equalValues('password','confirmPassword')],
+    }
+  ),
+    firstName: new FormControl('', {
+     validators: [Validators.required] }),
+    lastName: new FormControl('', {
+     validators: [Validators.required] }),
+    address: new FormGroup({
+      street: new FormControl('', {
+        validators: [Validators.required] }),
+       number: new FormControl('', {
+        validators: [Validators.required] }), 
+       postalCode: new FormControl('', {
+        validators: [Validators.required] }), 
+       city: new FormControl('', {
+        validators: [Validators.required] }), 
+    }),
+     role: new FormControl<'student' | 'teacher' | 'employee' 
+     | 'founder' | 'other'>('student', { validators: [Validators.required]}),
+  
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
+     agree: new FormControl(false, {validators:[Validators.required]}),
+    });
   onSubmit(){
-console.log(this.form);
+    if(this.form.invalid){
+      console.log("INVALID FORM");
+      return;
+    }
+     console.log(this.form);
   }
   onReset(){
-this.form.reset();
+     this.form.reset();
   }
 }
